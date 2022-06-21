2bc6026f6722d1f96380e9a0975b5a88fa8b7716
itzrilly <rilwanougarbarilly@gmail.com>
2022-06-21 14:52:01 +0100

    Bottom Tabs added

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './screens/HomeScreen';
+import { StyleSheet, Text, View } from 'react-native'
+import HomeScreen from './screens/HomeScreen'
+import SearchScreen from './screens/SearchScreen'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Entypo, FontAwesome } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <HomeScreen />
-    </View>
-  );
+
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={{
+          tabBarActiveTintColor: 'red',
+
+        }}>
+          <Tab.Screen name='Home' component={HomeScreen} options={{
+            tabBarIcon: () => <Entypo name="home" size={24} color="black" />
+          }} />
+          <Tab.Screen name='Search' component={SearchScreen} options={{
+            tabBarIcon: () => <FontAwesome name="search" size={24} color="black" />
+          }} />
+        </Tab.Navigator>
+    </NavigationContainer>
+
+  )
 }
 
 const styles = StyleSheet.create({
